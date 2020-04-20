3a60626335c72d48a52409e230276bcd72d563d8
murali-dell <murali.itservice@gmail.com>
2020-04-20 22:03:39 +0530

    Code Added

---
+++ src/main/java/com/social/imageApp/account/service/UserServiceImp.java
@@ -0,0 +1,47 @@
+package com.social.imageApp.account.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.social.imageApp.account.dao.UserDao;
+import com.social.imageApp.account.model.User;
+
+@Service("UserService")
+@Transactional
+public class UserServiceImp implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Transactional
+	@Override
+	public Boolean saveUser(User user) {
+		return null;		
+		
+	}
+	
+	@Override
+	public User getUser(int userId) {
+		return null;		
+
+	}
+
+	@Override
+	public List<User> listUser() {
+		return null;
+		
+	}
+
+	@Override
+	public void updateUser(int userId,User user) {		
+		
+	}
+
+	@Override
+	public void deleteUser(int userId) {
+		
+	}
+}
